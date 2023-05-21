c1a1bb40bfd09152df25cf141a1902727d30682d
Partho Roy <roypartho772@gmail.com>
2023-05-21 14:16:24 +0600

    data showed based on category

--- src/pages/Home/ShopByCategory/ShopByCategory.jsx
+++ src/pages/Home/ShopByCategory/ShopByCategory.jsx
@@ -1,19 +1,28 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import ToyCard from './ToyCard';
 const ShopByCategory = () => {
-    const [toys, setToys] = useState()
+    const [toys, setToys] = useState();
+    const [query, setQuery] = useState('Sports Car');
+    useEffect(() => {
+        fetch(`http://localhost:5000/subCategory/${query}`)
+        .then(res => res.json()).then(data => setToys(data))
+    }, [query])
+    console.log(toys);
     return (
         <div className='px-5 my-10 lg:px-24'>
             <div className='text-center'>
                 <h2 className='text-4xl'>Shop By Category</h2>
                 <p className='text-lg font-semibold text-[#757575] my-3'>Here are some awesome collection based on different category</p>
             </div>
-            <div className='text-center space-x-4'>
-                <button className='bg-blue-200 rounded-3xl hover:bg-blue-100 font-semibold px-8 py-3'>Sports Cars</button>
-                <button className='bg-blue-200 rounded-3xl hover:bg-blue-100 font-semibold px-8 py-3'>Trucks</button>
-                <button className='bg-blue-200 rounded-3xl hover:bg-blue-100 font-semibold px-8 py-3'>Regular Cars</button>
+            <div className='text-center space-y-3 space-x-4'>
+                <button onClick={()=> setQuery('Sports Car')} className='bg-blue-200 rounded-3xl hover:bg-blue-100 font-semibold px-8 py-3'>Sports Cars</button>
+                <button onClick={() => setQuery('Truck')} className='bg-blue-200 rounded-3xl hover:bg-blue-100 font-semibold px-8 py-3'>Trucks</button>
+                <button onClick={() => setQuery('Regular Car')} className='bg-blue-200 rounded-3xl hover:bg-blue-100 font-semibold px-8 py-3'>Regular Cars</button>
             </div>
-            <div>
-
+            <div className='grid grid-cols-1 lg:grid-cols-3 my-5 gap-5'>
+                {
+                    toys?.map(toy => <ToyCard key={toy._id} toy={toy}></ToyCard>)
+                }
             </div>
         </div>
     );
---
+++ src/pages/Home/ShopByCategory/ToyCard.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const ToyCard = ({ toy }) => {
+    const { _id, toyName, toyImg, subCategory,ratings, price, quantity, sellerName, sellerEmail, description } = toy;
+    return (
+        <div>
+            <div className="card bg-base-100 shadow-xl">
+                <figure className="px-2 pt-5">
+                    <img src={toyImg} alt="Shoes" className="rounded-xl h-48" />
+                </figure>
+                <div className="card-body flex flex-row justify-between items-center">
+                    <div>
+                        <h2 className="card-title">{toyName}</h2>
+                        <p>Ratings: {ratings}</p>
+                    </div>    
+                    <div>
+                        <button className='btn btn-secondary border-0 btn-xs'>View Details</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default ToyCard;
